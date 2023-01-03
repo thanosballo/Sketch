9979821882a0a86c6e33eb9fdc5466ab0ec12c01
thanosbalo <thanosbalogiannis@gmail.com>
2023-01-04 00:30:46 +0200

    changes made

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
 const container=document.querySelector(".container");
+const clear=document.querySelector(".clear-btn");
 let i=0;
 let gridselection=16*16;
 for (i=0;i<gridselection;i++){
     const div=document.createElement('div');
-    div.classList.add("border","box");
+    div.classList.add("box");
     container.appendChild(div);
 };
 
-const box=Array.from(document.querySelectorAll(".box"));
+const boxes=Array.from(document.querySelectorAll(".box"));
+boxes.forEach(div => {
+    div.addEventListener("mouseover",()=>{
+        
+        div.classList.add("grey");
+    });
+});
+clear.addEventListener("click",()=>{
+    boxes.forEach(div=>{
+        div.classList.remove("grey");
+    });
+});
